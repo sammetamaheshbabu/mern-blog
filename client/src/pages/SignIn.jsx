e6b12e758a79070584cb53a18f9628e2ecda3611
Sammeta Mahesh Babu <70381126+sammetamaheshbabu@users.noreply.github.com>
2024-06-21 21:20:41 +0530

    Update SignIn.jsx
    Displaying error message properly Before this, there was no display the error message

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -31,7 +31,7 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        dispatch(signInFailure(data.message));
+        return dispatch(signInFailure(data.message));
       }
 
       if (res.ok) {
